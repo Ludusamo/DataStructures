b1ca986f31797f5de56c01353ccac30e47ca2e2b
Brendan Horng <ludusamo@gmail.com>
2015-09-24 21:48:05 -0400

    Add Josephus implementation

--- FloJo/main.cpp
+++ FloJo/main.cpp
@@ -2,5 +2,9 @@
 #include "Link.h"
 
 int main() {
-
+	std::vector<int> deathOrder = josephus(5, 3);
+	
+	for (int i = 0; i < deathOrder.size(); i++) {
+		printf("%i\n", deathOrder[i]);
+	}
 }
--- FloJo/src/Link.cpp
+++ FloJo/src/Link.cpp
@@ -5,5 +5,31 @@ std::vector<int> loopTail(Link<int>* head) {
 }
 
 std::vector<int> josephus(int n, int k) {
+	// Creates circularly linked list in reverse order
+	Link<int> *beginning = new Link<int>(n);
+	Link<int> *nextLink = beginning;
+	for (int i = n - 1; i > 0; i--) {
+		Link<int> *link = new Link<int>(i, nextLink);
+		nextLink = link;
+	}
+	beginning->next = nextLink;
 
+	Link<int> *currentLink = beginning;
+	std::vector<int> deathOrder;
+	int counter = 0;
+	while (currentLink->next != currentLink) {
+		counter++;
+		if (counter == k) {
+			nextLink = currentLink->next;
+			currentLink->next = nextLink->next;	
+			deathOrder.push_back(nextLink->info);
+			counter = 0;
+			nextLink->next = 0;
+			delete nextLink;
+		} else currentLink = currentLink->next;
+	}
+	deathOrder.push_back(currentLink->info);
+
+	delete beginning;
+	return deathOrder;
 }
