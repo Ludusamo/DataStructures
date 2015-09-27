2b1c42df650d9c10b4cfe67b8c1551ac54c5f081
Brendan Horng <ludusamo@gmail.com>
2015-09-26 21:10:32 -0400

    Modify for loopTail to work with arbitrary type T

--- FloJo/header/Link.h
+++ FloJo/header/Link.h
@@ -22,10 +22,59 @@ struct Link {
 	Link *next;
 };
 
-Link<int32_t> *hasLoop(Link<int32_t>* head); // Returns a link in the loop
-int32_t determineLoopSize(Link<int32_t>* linkInLoop);
-int32_t determineTailSize(Link<int32_t>* head, int32_t loopSize);
-std::vector<int32_t> loopTail(Link<int32_t>* head);
+template<class T>
+Link<T> *hasLoop(Link<T>* head) {
+	Link<T> *turtle = head;
+	Link<T> *hare = head;
+	do {
+		if (hare->next == 0 || hare->next->next == 0 || turtle->next == 0) 
+			return 0;
+		turtle = turtle->next;
+		hare = hare->next->next;
+	} while (turtle != hare);
+	return hare;
+}
+
+template<class T>
+int32_t determineLoopSize(Link<T>* linkInLoop) {
+	Link<T> *counter = linkInLoop->next;
+	int32_t count = 1;
+	while (counter != linkInLoop) {
+		count++;
+		counter = counter->next;
+	}
+	return count;
+}
+
+template<class T>
+int32_t determineTailSize(Link<T>* head, int32_t loopSize) {
+	Link<T> *link1 = head;
+	Link<T> *link2 = head;
+	for (int32_t i = 0; i < loopSize; i++)
+		link2 = link2->next;
+
+	int32_t counter = 0;
+	while (link1 != link2) {
+		counter++;
+		link1 = link1->next;
+		link2 = link2->next;
+	}
+	return counter;
+}
+
+template<class T>
+std::vector<int32_t> loopTail(Link<T>* head) {
+	Link<T> *loopLink = hasLoop(head);
+	std::vector<int32_t> loopTailInfo(2);
+	if (loopLink != 0) {
+		int32_t loopSize = determineLoopSize(loopLink);
+		loopTailInfo[0] = loopSize;
+		int32_t tailSize = determineTailSize(head, loopSize);
+		loopTailInfo[1] = tailSize;
+	}
+	return loopTailInfo;
+}
+
 std::vector<int32_t> josephus(int32_t n, int32_t k);
 
 #endif // LINK_H
--- FloJo/main.cpp
+++ FloJo/main.cpp
@@ -1,32 +1,66 @@
 #include <cstdio>
 #include <climits>
+#include <stdlib.h>
+#include <time.h>
 
 #include "Link.h"
-
-#define CHAIN_LENGTH 1000000
-#define LOOP_LINK 9999
 
 typedef Link<int32_t> ILink;
 
 int main() {
-	printf("Floyd:\n");
-	ILink *ending = new ILink(CHAIN_LENGTH);
-	ILink *next = ending;
-	for (int i = CHAIN_LENGTH - 1; i > 0; i--) {
-		ILink *link = new ILink(i, next);
-		next = link;
-		if (i == LOOP_LINK) ending->next = link;
-	}
-	ILink *beginning = next;
+	FILE *floydTestCases, *josephusTestCases;
+	floydTestCases = fopen("res/floyd.in", "w");
+	josephusTestCases = fopen("res/josephus.in", "w");
 
-	std::vector<int32_t> loopTailInfo = loopTail(beginning);
-	printf("<%i, %i>\n", loopTailInfo[0], loopTailInfo[1]);
+	printf("Floyd:\n");
+	for (int x = 0; x <= 1000; x++) {
+		srand(time(NULL));
+		int32_t CHAIN_LENGTH = rand() % (1000 * (x + 1)); 
+		srand(time(NULL));
+		CHAIN_LENGTH += rand() % 1000;
+		srand(time(NULL));
+		int32_t LOOP_LINK = (rand() % CHAIN_LENGTH) - 1;
+
+
+		//printf("%i %i\n", CHAIN_LENGTH, LOOP_LINK);
+		ILink *ending = new ILink(CHAIN_LENGTH);
+		ILink *next = ending;
+		for (int32_t i = CHAIN_LENGTH - 1; i > 0; i--) {
+			ILink *link = new ILink(i, next);
+			next = link;
+			if (i == LOOP_LINK) ending->next = link;
+		}
+		ILink *beginning = next;
+
+		std::vector<int32_t> loopTailInfo = loopTail(beginning);
+		//printf("<%i, %i>\n", loopTailInfo[0], loopTailInfo[1]);
+		fprintf(floydTestCases, "%i %i %i %i\n", CHAIN_LENGTH, LOOP_LINK, loopTailInfo[0], loopTailInfo[1]);
+		ending->next = 0;
+		delete beginning;
+	}
 
 	printf("\nJosephus:\n");
-	std::vector<int32_t> deathOrder = josephus(100, 3);
-	
+	for (int i = 0; i < 99; i++) {
+		srand(time(NULL));
+		int32_t n = rand() % (10000 * (i + 1)); 
+		srand(time(NULL));
+		int32_t k = rand() % 1000; 
+
+		std::vector<int32_t> deathOrder = josephus(n, k);
+		
+		fprintf(josephusTestCases, "%i %i ", n, k);
+		for (int32_t i = 0; i < deathOrder.size(); i++) {
+			fprintf(josephusTestCases, "%i ", deathOrder[i]);
+		}
+		fprintf(josephusTestCases, "\n");
+	}
+	std::vector<int32_t> deathOrder = josephus(1000000, 17);
+		
+	fprintf(josephusTestCases, "%i %i ", 1000000, 17);
 	printf("Death Order:\n");
 	for (int32_t i = 0; i < deathOrder.size(); i++) {
+		fprintf(josephusTestCases, "%i ", deathOrder[i]);
 		printf("%i\n", deathOrder[i]);
 	}
+	fprintf(josephusTestCases, "\n");
 }
