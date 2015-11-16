31f45a861547da4dc325ef4c1330a8f01879683f
Brendan Horng <brendan.horng@ludusamo.com>
2015-11-16 15:09:41 -0500

    Add minor changes

--- BinaryTree/src/tree.cpp
+++ BinaryTree/src/tree.cpp
@@ -17,11 +17,38 @@ Tree::Node::Node(const Entry& e, Node *l, Node *r, size_t n) {
 	this->n = n;
 }
 
+Tree::Node::~Node() {
+	
+}
+
 void Tree::Node::kill(Node *p) {
 	if (!p) return;
 	Tree::Node::kill(p->l);
 	Tree::Node::kill(p->r);
-	kill(p);
+	delete p;
+}
+
+Tree::It::It(Node *p, const stack<Node*>& s) {
+	this->p = p;
+	this->s = s;
+}
+
+Tree::It::It(const It& i) {
+	this->p = i.p;
+	this->s = i.s;	
+}
+
+Tree::It::~It() {
+	for (int i = 0; i < s.size(); i++) s.pop();
+	p = NULL;
+}
+
+Tree::It Tree::It::operator++(int) {
+	p = s.top();	s.pop();
+}
+
+Tree::It& Tree::It::operator++() {
+
 }
 
 Tree::Tree() {
@@ -30,4 +57,29 @@ Tree::Tree() {
 
 Tree::~Tree() {
 	Node::kill(root);
+}
+
+size_t Tree::size() const {
+	return root->n;
+}
+
+bool Tree::empty() const {
+	return root;
+}
+
+bool Tree::set(const string& key, double val) {
+	Node *currentNode;
+	vector<Node*> unvisitedNodes = vector<Node*>();
+	unvisitedNodes.push_back(root);
+	while (unvisitedNodes.size() > 0) {
+		currentNode = unvisitedNodes[unvisitedNodes.size() - 1];
+		if (currentNode->e.key == key) {
+			currentNode->e.val = val;
+			return true;
+		}
+		unvisitedNodes.pop_back();
+		if (currentNode->l) unvisitedNodes.push_back(currentNode->l);
+		if (currentNode->r) unvisitedNodes.push_back(currentNode->r);
+	}
+	return false;
 }
